216bf3aeef5108ff4455267ae71d7b94a01b266c
ajuarezse <ajuarez.se@gmail.com>
2025-03-11 13:48:16 -0700

    add time divs

--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -1,5 +1,27 @@
+import React, { useState } from "react";
+import "./Countdown.css";
+
 const Countdown = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section id="main-countdown">
+        <div id="countdown-timer">
+          <div className="time-unit">
+            <span>Days</span>
+          </div>
+          <div className="time-unit">
+            <span>Hours</span>
+          </div>
+          <div className="time-unit">
+            <span>Minutes</span>
+          </div>
+          <div className="time-unit">
+            <span>Seconds</span>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Countdown;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,12 @@
 import "./Main.css";
-import Countdown from "../Countdown/Countdown"; // Import the Countdown component
+import Countdown from "../Countdown/Countdown";
 
 function Main() {
   return (
     <div>
       <section className="main__container">
         <main>
-          {/* Replace the countdown section with the Countdown component */}
           <Countdown />
-
-          {/* Seasonal Decor Section (unchanged) */}
           <section id="seasonal-decor">
             <div className="decor-items">
               <span className="decor">❄️</span>
